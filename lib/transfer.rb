68d98aa31dec8aadc011bc415f3c29ca5b57092a
Justin Davis <davisjustinw@gmail.com>
2019-01-16 04:36:46 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,6 +18,7 @@ class Transfer
       @sender.deposit(-@amount)
       @status = "complete"
     else
+      @status = "rejected"
       "Transaction rejected. Please check your account balance."
     end
     
